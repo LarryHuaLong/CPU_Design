a9dfefeccffa8ead129238d792dcd3fd129c31e3
Larry <709603507@qq.com>
2018-02-26 19:28:03 +0800

    added some files

---
+++ MIPS CPU/MIPS CPU.srcs/sources_1/new/alu.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: hualong
+// 
+// Create Date: 2018/02/26 11:43:58
+// Design Name: 
+// Module Name: alu
+// Project Name: cpu
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module alu(
+    input [31:0] X,
+    input [31:0] Y,
+    input [3:0] S,
+    output [31:0] Result,
+    output [31:0] Result2,
+    output OF,
+    output UOF,
+    output Equal
+    );
+    
+    
+    
+    
+endmodule
